3bcd36ab1f11edd7d58c45ab3c16ec27c147dfba
Jupiter_56 <65504097+Jupiter-56@users.noreply.github.com>
2020-12-20 20:00:41 +0100

    ⏱ Add a cooldown

--- Furnace/src/fr/jupidev/furnace/utils/Settings.java
+++ Furnace/src/fr/jupidev/furnace/utils/Settings.java
@@ -4,8 +4,8 @@ public class Settings {
 
 
     public static String PLUGIN_URL = "https://www.spigotmc.org/resources/80841";
-    public static String SUPPORT_DISCORD_URL = "https://discord.gg/jMmy6ng";
-    public static String VERSION = "1.5";
+    public static String SUPPORT_DISCORD_URL = "https://discord.gg/YyXPeA6Bw2";
+    public static String VERSION = "2.0";
     public static String NAME = "Furnace";
 
 
